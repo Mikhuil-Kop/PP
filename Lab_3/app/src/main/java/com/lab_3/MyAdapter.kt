9df0bb26add30f699250da3ffa22287ffccb4ee0
Mike Kopotov <mikekopotov@iMac-27.local>
2020-04-21 00:00:31 +0300

    Накиданы базовые классы

---
+++ Lab_3/app/src/main/java/com/lab_3/MyAdapter.kt
@@ -0,0 +1,32 @@
+package com.lab_3
+
+import android.content.Context
+import android.database.Cursor
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.layout_element.view.*
+
+class MyAdapter(val items : Cursor, val context: Context) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return items.count
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.layout_element, parent, false))
+
+    }
+    // Прогрузка элементов в адаптере
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //holder?.number?.text =
+        items.moveToPosition(position)
+        var string = items.getString(0) + ": " + items.getString(1) + ": " + items.getString(2)
+        holder?.number?.text = string
+    }
+    class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+        val number = view.item_textView
+    }
+}
